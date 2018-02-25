f93dabc4b0fe03ddad635cce8211d43b3bcc4d91
Yaroslav O. Dmitriv <y.o.dmitriv@gmail.com>
2018-02-25 13:51:33 +0300

    Start learning proj

---
+++ ExerciseOne.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function toDEC( dec ) {
+
+    //определяем переменные: вывод, длинна двоичного числа, единица
+    let out = 0, len = dec.length, bit = 1;
+
+    //постепенно идем по числам исползуя декримент после использования
+    while( len-- ) {
+        //для проверки используем выражение в котором числа поданные в функцию должны быть единицей или нулем
+        if (dec[len]<=1) {
+            out += dec[ len ] === "1" ? bit : 0;
+            bit <<= 1;
+        }
+        else
+            out = undefined;
+        
+    }
+    return out;
+}
+console.log(toDEC('0111011101101'));
+
