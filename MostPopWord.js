acb4fa690f6208a3d5e8aa1ab671b335fb3306b7
Yaroslav O. Dmitriv <y.o.dmitriv@gmail.com>
2018-03-20 23:25:12 +0300

    Rebuilt task

--- MostPopWord.js
+++ MostPopWord.js
@@ -3,49 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function wordSpec (word){
-    this.word = word;
+
+function wordSpec() {
+    this.word = '';
     this.rate = 0;
-}
-
-function stringObj(){
-    this.string = null;
-    this.arr = null;
-}
-
-stringObj.prototype.add = function(string){
-    this.string = string.replace(/[,.]/g, ' ').toLowerCase;
-    this.arr = string.split('');
-    console.log(this.arr);
 };
+
+function stringObj(string){
+    this.string = string.toLowerCase().replace(/,.\n/g);
+    this.arr = string.split(' ');
+    return this.arr;
+}
 
 stringObj.prototype.strParse = function(){
     let tested = [], complete = [], item;
     
     while(this.arr.length){
-        item = new wordSpec(this.arr.shift());
-        if (tested.indexOf(item) === -1) {
-      // добавляем в список
-            tested.push(item);
-      // и ищем дубли
-            if (this.arr.indexOf(item) >= 0) {
-                item.rate++;
-            }
-        let fin = complete.push(item);
-        console.log(fin);
-    }
-        
+        console.log()
     }
     
 };
-let strob = new stringObj();
-strob.add("Sed tempus ipsum quis eros tempus lacinia Cras finibus lorem ut\n\
- lacinia egestas nunc nibh iaculis est convallis tincidunt mi mi sed nisl \n\
-Sed porttitor aliquam elit ullamcorper tincidunt arcu euismod quis Mauris congue\n\
- elit suscipit leo varius facilisis Cras et arcu sodales laoreet est vitae \n\
-pharetra orci Integer eget nulla dictum aliquet justo semper molestie neque \n\
-Maecenas bibendum lacus tincidunt auctor varius purus felis ullamcorper dui et \n\
-laoreet ligula ex et risus Donec eget fringilla nibh Cras congue tincidunt \n\
-accumsan Maecenas euismod eleifend elit ut rhoncus tortor sodales a Cras \n\
-egestas finibus lorem non tempor tincidunt aera");
-strob.strParse();
+let strob = new stringObj("Sed tempus ipsum quis eros tempus lacinia Cras finibus lorem ut lacinia egestas \
+nunc nibh iaculis est convallis tincidunt mi mi sed nisl Sed porttitor aliquam elit ullamcorper tincidunt arcu \
+euismod quis Mauris congue elit suscipit leo varius facilisis Cras et arcu sodales laoreet est vitae \
+pharetra orci Integer eget nulla dictum aliquet justo semper molestie neque \
+Maecenas bibendum lacus tincidunt auctor varius purus felis ullamcorper dui et laoreet ligula ex et risus Donec eget \
+fringilla nibh Cras congue tincidunt accumsan Maecenas euismod eleifend elit ut rhoncus tortor sodales a Cras egestas \
+finibus lorem non tempor tincidunt aera");
+console.log(strob)
