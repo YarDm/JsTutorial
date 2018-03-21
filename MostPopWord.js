c77cc6f49c59fb5e7046ef9b0f9abf1969f9de92
Yaroslav O. Dmitriv <y.o.dmitriv@gmail.com>
2018-03-21 18:53:23 +0300

    In process

--- MostPopWord.js
+++ MostPopWord.js
@@ -16,13 +16,24 @@ function stringObj(string){
 }
 
 stringObj.prototype.strParse = function(){
-    let tested = [], complete = [], item;
-    
-    while(this.arr.length){
-        console.log()
+
+    let approved = [], ws, array = this.arr;
+
+    for (let i = 0; i < array.length; i++){
+        ws = new stringObj();
+        ws.word = array[i];
+        ws.rate++;
+
+        if (ws.word == array[i]){
+            delete array[i];
+            ws.rate++;
+        }
+        approved.push(ws);
     }
-    
-};
+    return approved;
+
+    };
+
 let strob = new stringObj("Sed tempus ipsum quis eros tempus lacinia Cras finibus lorem ut lacinia egestas \
 nunc nibh iaculis est convallis tincidunt mi mi sed nisl Sed porttitor aliquam elit ullamcorper tincidunt arcu \
 euismod quis Mauris congue elit suscipit leo varius facilisis Cras et arcu sodales laoreet est vitae \
@@ -30,4 +41,4 @@ pharetra orci Integer eget nulla dictum aliquet justo semper molestie neque \
 Maecenas bibendum lacus tincidunt auctor varius purus felis ullamcorper dui et laoreet ligula ex et risus Donec eget \
 fringilla nibh Cras congue tincidunt accumsan Maecenas euismod eleifend elit ut rhoncus tortor sodales a Cras egestas \
 finibus lorem non tempor tincidunt aera");
-console.log(strob)
+strob.strParse();
