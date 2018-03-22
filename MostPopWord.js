54f680da6fd72f1067b7efd0e8c51a2dbf7879d6
Yaroslav O. Dmitriv <y.o.dmitriv@gmail.com>
2018-03-22 22:35:42 +0300

    Complete

--- MostPopWord.js
+++ MostPopWord.js
@@ -7,12 +7,11 @@
 function wordSpec() {
     this.word = '';
     this.rate = 0;
-};
+}
+function stringObj(text){
+    this.text = text.replace(/\n/g).toLowerCase();
+    this.arr = text.split(' ');
 
-function stringObj(string){
-    this.string = string.toLowerCase().replace(/,.\n/g);
-    this.arr = string.split(' ');
-    return this.arr;
 }
 
 stringObj.prototype.strParse = function(){
@@ -20,19 +19,38 @@ stringObj.prototype.strParse = function(){
     let approved = [], ws, array = this.arr;
 
     for (let i = 0; i < array.length; i++){
-        ws = new stringObj();
+        ws = new wordSpec();
         ws.word = array[i];
         ws.rate++;
 
-        if (ws.word == array[i]){
-            delete array[i];
-            ws.rate++;
+        let j = 0;
+        while (j < array.length){
+            if (ws.word === array[j]){
+                delete array[j];
+                ws.rate++;
+            }
+            else if (array.length === 0){
+                break;
+            }
+            j++
         }
+
         approved.push(ws);
     }
-    return approved;
+    for (let i = 0; i < approved.length; i++){
+        if (approved[i].word === undefined){
+            delete approved[i];
+        }
+    }
+    approved = approved.filter(function(n){ return n != undefined }).sort(sortFunc);
+    return approved[0];
 
     };
+
+function sortFunc(a, b) {
+    if (a.rate > b.rate) return -1;
+    if (a.rate < b.rate) return 1;
+}
 
 let strob = new stringObj("Sed tempus ipsum quis eros tempus lacinia Cras finibus lorem ut lacinia egestas \
 nunc nibh iaculis est convallis tincidunt mi mi sed nisl Sed porttitor aliquam elit ullamcorper tincidunt arcu \
@@ -41,4 +59,4 @@ pharetra orci Integer eget nulla dictum aliquet justo semper molestie neque \
 Maecenas bibendum lacus tincidunt auctor varius purus felis ullamcorper dui et laoreet ligula ex et risus Donec eget \
 fringilla nibh Cras congue tincidunt accumsan Maecenas euismod eleifend elit ut rhoncus tortor sodales a Cras egestas \
 finibus lorem non tempor tincidunt aera");
-strob.strParse();
+console.log(strob.strParse());
